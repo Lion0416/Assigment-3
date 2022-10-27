9175ac04f00fb68aef31ddbc7c0d1569dd104f88
Ziheng Liou <liouziheng@gmail.com>
2022-10-27 23:56:14 +1000

    update

--- Management/__init__.py
+++ Management/__init__.py
@@ -1,24 +1,36 @@
-from flack import Flack
+from flask import Flask 
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+
+db=SQLAlchemy()
 
 
 def create_app(): 
     app=Flask(__name__)
     app.debug = True
     app.secret_key="12345"
+
+    #Configue and initialise DB
+    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///travel123.sqlite'
+
+    #initialize db with flask app
     db.init_app(app)
 
+    #we use this utility module to display forms quickly
     bootstrap = Bootstrap5(app)
 
+    #initialize the login manager
     login_manager = LoginManager()
 
+
+    #set the name of the login function that lets user login
+    # in our case it is auth.login (blueprintname.viewfunction name)
     login_manager.login_view='auth.login'
     login_manager.init_app(app)
     
     from . import views
-    app.register_blueprint(views.bp)
+    app.register_blueprint(views.mainbp)
 
     from . import event
     app.register_blueprint(event.bp)
