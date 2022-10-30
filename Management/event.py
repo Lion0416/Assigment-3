3662793dba566d1ebf130ea88bc43e739cf3865e
Ziheng Liou <liouziheng@gmail.com>
2022-10-31 03:04:12 +1000

    update

--- Management/event.py
+++ Management/event.py
@@ -1,3 +1,4 @@
+from asyncio import events
 from flask import ( 
     Blueprint, flash, render_template, request, url_for, redirect
 ) 
@@ -5,3 +6,11 @@ from .models import Event
 from . import db, app
 from werkzeug.utils import secure_filename
 from flask_login import login_required, current_user
+
+
+bp = Blueprint('event', __name__, url_prefix='/event')
+
+@bp.route('/<eventid>')
+def show(id):
+
+    return render_template('destinations/show.html')
