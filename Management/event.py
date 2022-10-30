d5336663b2517d42df04a2e72e2a4c2f9e2d515b
Ziheng Liou <liouziheng@gmail.com>
2022-10-31 03:08:33 +1000

    update

--- Management/event.py
+++ Management/event.py
@@ -3,7 +3,7 @@ from flask import (
     Blueprint, flash, render_template, request, url_for, redirect
 ) 
 from .models import Event
-from . import db, app
+from . import db
 from werkzeug.utils import secure_filename
 from flask_login import login_required, current_user
 
