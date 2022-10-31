a290b12208b398322ba03e641c28c65ba3b8f974
Ziheng Liou <liouziheng@gmail.com>
2022-10-31 18:05:36 +1000

    update

--- Management/forms.py
+++ Management/forms.py
@@ -2,8 +2,8 @@
 from flask_wtf import FlaskForm
 from datetime import datetime, date
 from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField,DateField,TimeField,SelectField
-
-from wtforms.validators import InputRequired, Length, Email, EqualTo,DataRequired,ValidationError
+from flask_wtf.file import FileRequired, FileField, FileAllowed
+from wtforms.validators import InputRequired, Length, Email, EqualTo,DataRequired, FileRequired
 
 
 #creates the login information
@@ -24,7 +24,10 @@ class RegisterForm(FlaskForm):
     #submit button
     submit = SubmitField("Register")
 
+
+
 #this is for register form
+ALLOWED_FILE = {'PNG','JPG','png','jpg'}
 class EventForm(FlaskForm):
     event_name = StringField("Event Name", validators=[InputRequired('Enter event name')])
     event_start_date = DateField(label='startdate',format="%Y-%m-%d",validators = [DataRequired('please select event startdate')])
@@ -32,8 +35,13 @@ class EventForm(FlaskForm):
     event_start_time = TimeField(label='starttime')
     event_end_time = TimeField(label='endtime')
     event_type = SelectField(label='type')
-    event_state = SelectField(label='states')
-
+    event_state = SelectField(label='states', choices=['Upcoming', 'Selling Out', 'Sold Out'])
+    eventimage = FileField('Event Image', validators=[
+    FileRequired(message='Image cannot be empty'),
+    FileAllowed(ALLOWED_FILE, message='Only supports png,jpg,JPG,PNG')])
+    description = TextAreaField('Description', validators=[InputRequired()])
+    ticketPrice = StringField('Ticket Price', validators=[InputRequired()])
+    ticketQuantity = StringField('Ticket Quantity', validators=[InputRequired()])
 
 
 
