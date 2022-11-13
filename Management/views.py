0d6d97d852f9410dd7b628fc9bb65053b6c19df3
Ziheng Liou <liouziheng@gmail.com>
2022-11-13 16:50:13 +1000

    update

--- Management/views.py
+++ Management/views.py
@@ -1,13 +1,23 @@
 from flask import Blueprint, render_template
 from .models import Type,States
-from .models import Event,Comment
+from .models import Event,Comment,Type,States
 from .forms import EventForm,CommentForm
+from.import db
+from sqlalchemy.orm import sessionmaker
+
+
 
 mainbp = Blueprint('main', __name__)
 
 @mainbp.route('/')
 def index():
-    events = Event.query.all()
+    
+    #events = Event.query.all()
+    events = db.session.query(Event, Type, States).select_from(Event).join(Type).join(States).all()
+    
+    print(events)
+    
+    
     form = EventForm()
     form.event_type.choices = [(type.typeid,type.type) for type in Type.query.all()]
     form.event_state.choices = [(states.statesid,states.states) for states in States.query.all()]
